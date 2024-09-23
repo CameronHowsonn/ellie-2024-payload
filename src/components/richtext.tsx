3b515e5d12f6c2ef197b2db35a961948835c9903
Cameron Howson <cameron.howson@sidigital.co>
2024-09-23 20:38:28 +0100

    fix build error

--- src/components/richtext.tsx
+++ src/components/richtext.tsx
@@ -1,32 +1,31 @@
 import styled from 'styled-components'
 import type { Props } from './text'
-import { layout, space, position, typography, border, color } from 'styled-system'
+import { layout, space, position, typography, border } from 'styled-system'
+import Stack from '@/components/stack'
 export interface RichTextProps extends Props {
   text: any
-  className?: string
 }
 
-const RichText: React.FC<RichTextProps> = ({ text, color, className }, rest) => {
+const RichText: React.FC<RichTextProps> = ({ text }) => {
   return (
-    <RichTextSpan
-      dangerouslySetInnerHTML={{ __html: text }}
-      className={`animate ${className}`}
-      color={color}
-      {...rest}
-    />
+    <RichTextSpan dangerouslySetInnerHTML={{ __html: text }} className="animate-opaque" gap={4} />
   )
 }
 
 export default RichText
 
-export const RichTextSpan = styled.span<Props>`
+export const RichTextSpan = styled(Stack)<Props>`
   ${layout}
   ${space}
   ${position}
   ${typography}
   ${border}
-  ${color}
   font-size: var(--p-font-size);
+  opacity: 0.5;
   line-height: 1.5;
   display: block;
+
+  > * + * {
+    margin-top: 1.5rem;
+  }
 `
